2a0dce396a38421a6b9f9b4798ca63dc8a6f64ec
i0Exception <AndrewOMagnuson@gmail.com>
2022-05-17 23:05:48 -0500

    Added comments for all config options

--- src/main/java/com/i0dev/plugin/infiniteobsidian/config/GeneralConfig.java
+++ src/main/java/com/i0dev/plugin/infiniteobsidian/config/GeneralConfig.java
@@ -14,19 +14,19 @@ public class GeneralConfig extends AbstractConfiguration {
     }
 
     protected void setValues() {
-        config.set("itemDisplayName", "&c&lInfinite Obsidian");
+        config.set("itemDisplayName", "&c&lInfinite Obsidian", "The display name of the Infinite Obsidian block.");
         config.set("itemLore", Arrays.asList(
                 "",
                 "&7Place this block to never run out of obsidian",
                 "&7It will charge &a${price} &7each time its placed"
-        ));
-        config.set("itemMaterial", "BEDROCK");
-        config.set("itemGlow", true);
+        ), "The lore of the infinite obsidian block");
+        config.set("itemMaterial", "BEDROCK", "This is the material for the item that the player will hold and use to click with, this is NOT what will be placed.");
+        config.set("itemGlow", true, "Weather or not the item in the players hand should glow.");
 
-        config.set("pricePerPlace", 1000);
-        config.set("placeMaterial", "OBSIDIAN");
-        config.set("messagePriceEveryXSeconds", 45);
-        config.set("disablePlacingInSystemFactions", true);
+        config.set("pricePerPlace", 1000, "How much it costs to place an Infinite Obsidian.");
+        config.set("placeMaterial", "OBSIDIAN", "This is the material for the block that ACTUALLY gets placed down.");
+        config.set("messagePriceEveryXSeconds", 45, "How often to send message about how much money they recently spent.");
+        config.set("disablePlacingInSystemFactions", true, "Should placing Infinite Obsidian blocks be disabled in system factions (WarZone and SafeZone)");
 
     }
 }
